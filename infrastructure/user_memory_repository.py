ff65173bd6f48ddb3306a89263a27e0e55f05cb8
Vitolofs7 <vitologamer71@gmail.com>
2026-01-30 20:28:19 +0000

    added user_memory_repository doc

--- infrastructure/user_memory_repository.py
+++ infrastructure/user_memory_repository.py
@@ -4,17 +4,43 @@ from domain.user_repository import UserRepository
 
 
 class UserMemoryRepository(UserRepository):
+    """In-memory implementation of the UserRepository interface."""
+
     def __init__(self):
+        """Initializes an empty in-memory data store for users."""
         self._data = {}
 
     def save(self, user):
+        """Saves a user in memory.
+
+        Args:
+            user: The User instance to save.
+        """
         self._data[user.user_id] = user
 
     def get(self, user_id):
+        """Retrieves a user by their ID.
+
+        Args:
+            user_id: The ID of the user to retrieve.
+
+        Returns:
+            The User instance if found, or None otherwise.
+        """
         return self._data.get(user_id)
 
     def list(self):
+        """Returns all users stored in memory.
+
+        Returns:
+            A list of all User instances.
+        """
         return list(self._data.values())
 
     def delete(self, user_id):
+        """Deletes a user from memory by their ID.
+
+        Args:
+            user_id: The ID of the user to delete.
+        """
         self._data.pop(user_id, None)
